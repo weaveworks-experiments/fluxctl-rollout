72f68b95046860b600fdda73a3a7f4df2368e860
Luke Marsden <me@lukemarsden.net>
2017-08-26 23:36:19 +0100

    Update 'manual unit test'...

--- prototype.py
+++ prototype.py
@@ -22,19 +22,18 @@ def main():
 action      | deployment:replicas,traffic%,[* = primary]
 --------------------------------------------------------
 init(v2)    | front-end*:n,100%
-stage()     | front-end*:n,100%, front-end--v2:n,0%
-Syncing: <Deployment front-end, primary=True, percent=5, replicas=7> <Deployment front-end--v2, primary=False, percent=95, replicas=7>
+canary()    | front-end*:n,95%, front-end--v2:n,5%
+Syncing: <Deployment front-end, primary=True, percent=95, replicas=7> <Deployment front-end--v2, primary=False, percent=5, replicas=7>
 
 release()   | front-end:0,0%, front-end--v2*:n,100%
 Syncing: <Deployment front-end, primary=False, percent=0, replicas=0> <Deployment front-end--v2, primary=True, percent=100, replicas=7>
 
-init(v3)    | front-end:0,0%, front-end--v2*:n,100%
-stage()     | front-end:0,0%, front-end--v2*:n,100%, front-end--v3:n,0%
-Syncing: <Deployment front-end--v2, primary=True, percent=0, replicas=7> <Deployment front-end--v3, primary=False, percent=100, replicas=7>
-
-release()   | front-end:0,0%, front-end--v2:0,0%, front-end--v3*:n,100%
+init(v3)    | front-end--v2*:n,100%
+bluegreen() | front-end--v2*:n,100%, front-end--v3:n,0%
 Syncing: <Deployment front-end--v2, primary=True, percent=100, replicas=7> <Deployment front-end--v3, primary=False, percent=0, replicas=7>
 
+abort()     | front-end--v2*:n,100%, front-end--v3:0,0%
+Syncing: <Deployment front-end--v2, primary=True, percent=100, replicas=7> <Deployment front-end--v3, primary=False, percent=0, replicas=0>
 """
 
 if __name__ == "__main__":
